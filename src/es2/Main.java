616f559bf7df80095446b05e48b09eb55240f57e
DiegoBas17 <167855623+DiegoBas17@users.noreply.github.com>
2024-07-30 16:26:51 +0200

    es2

--- src/es2/Main.java
+++ src/es2/Main.java
@@ -1,14 +1,48 @@
 package es2;
 
-import java.util.Scanner;
-import java.util.Set;
-import java.util.TreeSet;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         Set<Integer> numeriRandom = new TreeSet<>();
+        Random random = new Random();
+        System.out.println("inserisci il numero di numeri casuali che vuoi visualizzare");
+        int numeriDaVisualizzare = scanner.nextInt();
+        for (int i = 0; i < numeriDaVisualizzare; i++) {
+            int numeroCasuale = random.nextInt(100);
+            numeriRandom.add(numeroCasuale);
+        }
+        System.out.println("Numeri random ordinati:");
+        System.out.println(numeriRandom);
 
-        
+        List<Integer> listaNumeri = new ArrayList<>(numeriRandom);
+        List<Integer> listaInversa = visualizzaInverso(listaNumeri);
+        System.out.println("Lista al contrario: " + listaInversa);
+
+        stampaValoriParioDispari(listaNumeri, true);
+        stampaValoriParioDispari(listaNumeri, false);
+    }
+
+    public static List<Integer> visualizzaInverso(List<Integer> listIniziale) {
+        List<Integer> listaInversa = new ArrayList<>();
+        for (int i = listIniziale.size() - 1; i >= 0; i--) {
+            listaInversa.add(listIniziale.get(i));
+        }
+        return listaInversa;
+    }
+
+    public static void stampaValoriParioDispari(List<Integer> lista, boolean pariDispari) {
+        if (pariDispari) {
+            System.out.println("I valori pari sono:");
+            for (int i = 0; i < lista.size(); i += 2) {
+                System.out.println(lista.get(i));
+            }
+        } else {
+            System.out.println("I valori in posizioni dispari:");
+            for (int i = 1; i < lista.size(); i += 2) {
+                System.out.println(lista.get(i));
+            }
+        }
     }
 }
---
+++ src/es3/Main.java
@@ -0,0 +1,14 @@
+package es3;
+
+import java.util.HashMap;
+
+public class Main {
+    public static void main(String[] args) {
+        HashMap<String, Integer> rubrica = new HashMap<>();
+
+    }
+
+    public void aggiungiNuovoNumero(String nome, int numero) {
+
+    }
+}
